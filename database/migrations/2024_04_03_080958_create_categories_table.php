8eee8bae1a6cca9ffa65c3a311bd6d40ca52cc3f
Furkan Can Bayrak <furkancanb957@gmail.com>
2024-04-20 17:39:15 +0300

    tasks ve categories tablosuna yeni alanlar eklendi

--- database/migrations/2024_04_03_080958_create_categories_table.php
+++ database/migrations/2024_04_03_080958_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_active')->default(1)->comment('0-inaktif, 1-aktif');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
